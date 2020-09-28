5045c6f19ebab8a4f239f3aa037df56395ae7ee2
jlwoodwa <jlwoodwa@caltech.edu>
2020-09-28 21:46:22 +0000

    further bugfixes and investigations

--- src/Language/Aglet.hs
+++ src/Language/Aglet.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections #-}
+
+-- TODO: nested lambdas don't work!
+-- d <- flip a initCtx $ sApp [sApp [SLambda ["x"] $ SLambda ["y"] $ SVar "x", SLit 1], SLit 2]
 
 module Language.Aglet where
 
@@ -9,13 +14,15 @@ import Data.Foldable (toList)
 import Data.Map.Strict (Map, empty, fromList, insert, member, (!?))
 import Data.Maybe (fromMaybe)
 import Data.Sequence (Seq (Empty, (:<|), (:|>)), scanl, singleton)
+import Data.Sequence qualified as S (fromList)
 import Data.Text (Text, intercalate, pack, unpack)
+import Debug.Trace (trace, traceShow, traceShowId)
 import Prelude hiding (scanl)
 
 newtype Context = Context (Seq Frame) deriving (Show)
 
 lookContext :: Context -> Ident -> Maybe Pneuma
-lookContext (Context ctx) ident = go ctx
+lookContext (Context ctx) ident = let res = go ctx in trace ("looking up " <> unpack ident <> " in " <> show ctx <> " results in " <> show res) $ res
   where
     go :: Seq Frame -> Maybe Pneuma
     go Empty = Nothing
@@ -60,7 +67,6 @@ data Soma
 data Pneuma
   = PLit Int
   | PNil
-  | PSymb Ident
   | PQuot Soma
   | PFun Ident ([Pneuma] -> Context -> (Pneuma, Context))
 
@@ -69,12 +75,16 @@ instance Show Pneuma where
   show (PNil) = "PNil"
   show (PFun name _) = "[function: " <> unpack name <> "]"
 
+rebreathe :: Pneuma -> Context -> (Pneuma, Context)
+rebreathe (PQuot s) ctx = breathe s ctx
+rebreathe els ctx = (els, ctx)
+
 breathe :: Soma -> Context -> (Pneuma, Context)
 breathe (SLit x) ctx = (PLit x, ctx)
 breathe SNil ctx = (PNil, ctx)
 breathe (SVar var) ctx =
   (,ctx)
-    . fromMaybe PNil -- correct signalling of exception
+    . fromMaybe (trace ("LOOKUP FAILED OF VARIABLE " <> unpack var) $ PNil) -- correct signalling of exception
     $ lookContext ctx var
 breathe (SQuot s) ctx = (PQuot s, ctx)
 breathe (SLambda idents body) ctx =
@@ -83,9 +93,9 @@ breathe (SLambda idents body) ctx =
     $ \pneums (Context ctx') ->
       case breathe body
         . Context
-        $ Frame (fromList $ zip idents pneums)
-          :<| ctx' of
-        b@(_, Context Empty) -> b
+        $ Frame (traceShowId $ fromList $ zip idents pneums)
+          :<| ctx' of -- THIS MUST BE CTX, NOT CTX'!!!!
+        b@(_, Context Empty) -> error "this should be impossible " -- b
         (pne, Context (trash :<| old)) -> (pne, Context old)
 breathe (SApp Empty) ctx = (PNil, ctx) -- should be `error`?
 breathe (SApp somas) ctx =
@@ -96,7 +106,7 @@ breathe (SApp somas) ctx =
     (Empty, _) -> error "Guard above prevents this case"
     (f :<| xs, ctx') -> case f of
       PFun _ fun -> fun (toList xs) ctx'
-      _ -> (PNil, ctx') -- should be `error`?
+      _ -> trace "TRIED TO APPLY NON-FUNCTION" (PNil, ctx') -- should be `error`?
   where
     dyeLift comp below c =
       let (pne, c') = comp c
@@ -118,5 +128,80 @@ initCtx =
       [ ( "+",
           PFun "builtin +" \args ctx ->
             (,ctx) . PLit . sum . map (\(PLit x) -> x) $ args
+        ),
+        ("-", PFun "builtin -" \[PLit x1, PLit x2] ctx -> (,ctx) . PLit $ x1 - x2),
+        ( "*",
+          PFun "builtin *" \args ctx ->
+            traceShow args $ (,ctx) . PLit . sum . map (\(PLit x) -> x) $ args
+        ),
+        ( "if",
+          PFun "builtin if" \[bool, arm1, arm2] ctx -> flip rebreathe ctx case bool of
+            PNil -> arm2
+            _ -> arm1
+        ),
+        ( "=",
+          PFun "builtin =" \args ctx ->
+            (,ctx) $
+              if let allEqual :: Eq a => [a] -> Bool
+                     allEqual [] = True
+                     allEqual (x : xs) = all (== x) xs
+                  in allEqual (map (\(PLit x) -> x) args)
+                then PLit 0
+                else PNil
+        ),
+        ( "Y",
+          evalInEmptyCtx $
+            SLambda ["f"] $
+              SApp $
+                S.fromList
+                  [ SLambda ["x"] $ SApp $ S.fromList [SVar "x", SVar "x"],
+                    SLambda ["x"] $
+                      SApp $
+                        S.fromList
+                          [ SVar "f",
+                            SLambda ["y"] $
+                              SApp $
+                                S.fromList
+                                  [ SApp $ S.fromList $ [SVar "x", SVar "x"],
+                                    SVar "y"
+                                  ]
+                          ]
+                  ]
+        ),
+        ( "def",
+          PFun "builtin def" $ flip \case
+            Context Empty -> const (PNil, Context Empty)
+            Context (Frame skim :<| rest) -> \[PQuot (SVar x), defn] ->
+              (PNil, Context $ Frame (insert x defn skim) :<| rest)
+        ),
+        ( "alFact",
+          evalInEmptyCtx $
+            SLambda ["f"] $
+              SLambda ["n"] $
+                sApp
+                  [ SVar "if",
+                    sApp [SVar "=", SVar "n", SLit 0],
+                    SLit 1,
+                    sApp
+                      [ SVar "*",
+                        SVar "n",
+                        sApp
+                          [ SVar "f",
+                            sApp
+                              [ SVar "-",
+                                SVar "n",
+                                SLit 1
+                              ]
+                          ]
+                      ]
+                  ]
         )
       ]
+
+emptyCtx :: Context
+emptyCtx = Context Empty
+
+evalInEmptyCtx :: Soma -> Pneuma
+evalInEmptyCtx s = fst $ breathe s emptyCtx
+
+sApp = SApp . S.fromList
